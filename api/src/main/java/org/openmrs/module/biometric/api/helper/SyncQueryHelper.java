bc63c844264821b48481f93f49b6337c21a5920c
Piotr Wargulak <pwargulak@soldevelo.com>
2025-08-14 15:47:31 +0200

    Corrected total count calculation for Person Images (handled voided attributes)

--- api/src/main/java/org/openmrs/module/biometric/api/helper/SyncQueryHelper.java
+++ api/src/main/java/org/openmrs/module/biometric/api/helper/SyncQueryHelper.java
@@ -18,59 +18,50 @@ public final class SyncQueryHelper {
   private static final String TEMPLATES_SQL = "SELECT dbid, TO_BASE64(template) template FROM"
       + " iris_templates where dbid in (:patientIdentifierList) ";
 
-  private static final String INNER_JOIN_PERSON_ATTRIBUTES = "inner join person.attributes as attribute ";
-  private static final String ATTRIBUTE_TYPE_FILTER = " where attribute.attributeType.name = :attributeTypeName ";
-
-  private static final String LOCATION_ATTRIBUTE_FILTER = " where pa.attributeType.name='LocationAttribute' and pa.value in (:locations)";
-  private static final String AND_PERSON_ID_IN = " and person.id in ";
-  private static final String SELECT_PERSON = " select p.id from Person as p ";
-  private static final String INNER_JOIN_ATTRIBUTES = " inner join p.attributes as pa ";
   private static final String SYNC_IGNORED_COUNT_QUERY =
-      "select count(person.id) from Person as person  "
-          + INNER_JOIN_PERSON_ATTRIBUTES
-          + ATTRIBUTE_TYPE_FILTER
-          + AND_PERSON_ID_IN
-          + "("
-          + SELECT_PERSON
-          + INNER_JOIN_ATTRIBUTES
-          + LOCATION_ATTRIBUTE_FILTER
-          + " and attribute.value = :deviceId"
-          + ") ";
+      "select count(person.id) "
+          + "from Person as person "
+          + "inner join person.attributes as attribute "
+          + " where attribute.voided = 0 and attribute.attributeType.name = :attributeTypeName "
+          + " and person.id in ("
+          + "   select p.id from Person as p "
+          + "   inner join p.attributes as pa "
+          + "   where pa.voided = 0 and pa.attributeType.name='LocationAttribute' "
+          + "     and pa.value in (:locations)"
+          + "     and attribute.value = :deviceId)";
 
   private static final String SYNC_TOTAL_COUNT_QUERY =
-      "select  person.voided, count(person.voided) as count from Person as person  "
-          + INNER_JOIN_PERSON_ATTRIBUTES
-          + ATTRIBUTE_TYPE_FILTER
-          + AND_PERSON_ID_IN
-          + "("
-          + SELECT_PERSON
-          + INNER_JOIN_ATTRIBUTES
-          + LOCATION_ATTRIBUTE_FILTER
-          + ") "
-          + " group by person.voided ";
+      "select person.voided, count(person.voided) as count from Person as person "
+          + "inner join person.attributes as attribute "
+          + "where attribute.voided = 0 and attribute.attributeType.name = :attributeTypeName "
+          + "  and person.id in ("
+          + "    select p.id from Person as p "
+          + "    inner join p.attributes as pa "
+          + "    where pa.voided = 0 and pa.attributeType.name='LocationAttribute' "
+          + "      and pa.value in (:locations)) "
+          + "group by person.voided ";
 
   private static final String SYNC_TEMPLATES_SQL =
-      "select person.uuid as uuid, pi.identifier as identifier, person.voided as voided"
-          + " ,person.dateChanged as dateModified from Person as person "
-          + " inner join person.identifiers as pi "
-          + INNER_JOIN_PERSON_ATTRIBUTES
-          + ATTRIBUTE_TYPE_FILTER
-          + AND_PERSON_ID_IN
-          + "("
-          + SELECT_PERSON
-          + INNER_JOIN_ATTRIBUTES
-          + LOCATION_ATTRIBUTE_FILTER
-          + ") ";
+      "select person.uuid as uuid, pi.identifier as identifier, "
+          + "person.voided as voided,person.dateChanged as dateModified "
+          + "from Person as person "
+          + "inner join person.identifiers as pi "
+          + "inner join person.attributes as attribute "
+          + "where attribute.voided = 0 and attribute.attributeType.name = :attributeTypeName "
+          + "and person.id in ("
+          + "  select p.id from Person as p "
+          + "  inner join p.attributes as pa "
+          + "  where pa.voided = 0 and pa.attributeType.name='LocationAttribute' "
+          + "    and pa.value in (:locations))";
 
   private static final String SYNC_IMAGES_SQL = "select person from Person as person  "
-      + INNER_JOIN_PERSON_ATTRIBUTES
-      + ATTRIBUTE_TYPE_FILTER
-      + AND_PERSON_ID_IN
-      + "("
-      + SELECT_PERSON
-      + INNER_JOIN_ATTRIBUTES
-      + LOCATION_ATTRIBUTE_FILTER
-      + ") ";
+      + "inner join person.attributes as attribute "
+      + "where attribute.voided = 0 and attribute.attributeType.name = :attributeTypeName "
+      + "  and person.id in ("
+      + "    select p.id from Person as p "
+      + "    inner join p.attributes as pa "
+      + "    where pa.voided = 0 and pa.attributeType.name='LocationAttribute' "
+      + "      and pa.value in (:locations))";
 
   private SyncQueryHelper() {
 
